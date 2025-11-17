25bf1adfa207d0f2c549a28cb1834f59ea190d4c
7amok4a <7amok4a.9960@gmail.com>
2025-11-17 19:38:28 +0200

    add middleware for security

--- src/server.js
+++ src/server.js
@@ -6,15 +6,28 @@ import notFoundMiddleware from "./middleware/notFoundHandler.js" ;
 import errorHandlerMiddleware from "./middleware/errorHandler.js" ; 
 import createConnectionDb from "./database/connect.js";
 import authMiddleware from "./middleware/authentication.js";
+
+//security 
+import helmet from "helmet" ; 
+//import xss from "xss-clean" ; // unused package  
+import rateLimit from "express-rate-limit";
+import cors from "cors" ; 
 dotenv.config() ; 
 
 const PORT = process.env.PORT ;
 const DB_URL  = process.env.DB_URL ; 
 const app = express() ; 
 
+app.set("trust proxy", 1);
+app.use(rateLimit({
+    windowMs : 15*60*1000 , 
+    max : 100 , // 100 request for per windowMs
+}))
+app.use(express.json()) ;
+app.use(helmet()) ;  
+app.use(cors()) ; 
+//app.use(xss()) ; 
 
-
-app.use(express.json()) ; 
 app.use("/api/v1/auth" , routerAuth) ; 
 app.use("/api/v1/jobs" , authMiddleware, routerJob);
 app.use(notFoundMiddleware) ; 
